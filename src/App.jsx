793254466d2d71f4e8eedb6d5e9ccb34bb2ff6f9
Laurie Yeh <laurie8925@gmail.com>
2024-02-11 14:39:52 -0800

    added scss and added variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import AppRouter from '../router/AppRouter'
+import "./styles/styles.scss"; 
 
 function App() {
   const [count, setCount] = useState(0)
