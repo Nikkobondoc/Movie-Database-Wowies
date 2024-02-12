87d2eae0a5be84a9644d36221cdf12c182fb6a64
Nikko <nikkobondoc1120@gmail.com>
2024-02-11 16:52:12 -0800

    added to the select part of home

--- pages/PageHome.jsx
+++ pages/PageHome.jsx
@@ -4,9 +4,10 @@ import Movie from '../components/Movie';
 const PageHome = () => {
   const [moviesList, setMoviesList] = useState([]);
 
-  const fetchMovies = useCallback(async () => {
+  const fetchMovies = useCallback(async (query = "popular") => {
     try {
-      const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=d616641b0feb479ee6b4cb90a886cb1f");
+      console.log(query)
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${query}?api_key=d616641b0feb479ee6b4cb90a886cb1f`);
       if (response.ok) {
         const data = await response.json();
         const selectedMovies = data.results.slice(0, 12);
@@ -33,17 +34,19 @@ const PageHome = () => {
       <h2>PUT FEATURE MOVIE HERE</h2>
     </div>
     <div className='movie-list'>
-      <select name="movie" id="movie">
-        <option value="now-playing">Now Playing</option>
-        <option value="popular">Popular</option>
-        <option value="top-rated">Top Rated</option>
-        <option value="Upcoming">Upcoming</option>
+      <select name="movie" id="movie" onChange={(e) => fetchMovies(e.target.value)}>
+        {/* on change, call the fetch movie function and change the query according to the valu */}
+        {/* add onclick listener -> call fetch movie function and call the respective filter of each option*/}
+        <option value="popular" >Popular</option>
+        <option value="now_playing" >Now Playing</option>
+        <option value="top_rated" >Top Rated</option>
+        <option value="upcoming" >Upcoming</option>
       </select>
     </div>
       <div>Popular Movies</div>
-    <div className='popular-home'>
+    <div className='home-grid'>
       {moviesList.map(movie => (
-        <Movie movieCard={movie} />
+        <Movie key={movie.id} movieCard={movie} />
         ))}
     </div>
     
