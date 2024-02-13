3d3fd33ee2126c84e440a2529eef32c0e2533a5f
Nikko <nikkobondoc1120@gmail.com>
2024-02-12 18:15:02 -0800

    fixed up the about page and started to work on hover state for home page

--- components/Movie.jsx
+++ components/Movie.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Movie = ({movieCard}) => {
   return (
-    <div className='popular-movie'>
-      <img key={movieCard.id} src={`https://image.tmdb.org/t/p/w200/${movieCard.poster_path}`} alt={movieCard.title} />
-      <p>{movieCard.title}</p>
-      <p>{movieCard.release_date}</p>
-      <p>{movieCard.vote_average}</p>
-    </div>
+      <div className='popular-movie'>
+        <div className='movie-image-wrap'>
+        <img key={movieCard.id} src={`https://image.tmdb.org/t/p/w200/${movieCard.poster_path}`} alt={movieCard.title} />
+        </div>
+        <p>{movieCard.title}</p>
+        <p>{movieCard.release_date}</p>
+        <p>{movieCard.vote_average}</p>
+      </div>
   )
 }
 
