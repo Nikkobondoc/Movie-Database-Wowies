45560487ce6c96643e6ade5e61d41c3252f77ed5
Nikko <nikkobondoc1120@gmail.com>
2024-02-12 23:09:51 -0800

    added button to hover state

--- components/Movie.jsx
+++ components/Movie.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const Movie = ({movieCard}) => {
-  return (
-<div className='popular-movie'>
-  <div className='movie-image-wrap'>
-    <img key={movieCard.id} src={`https://image.tmdb.org/t/p/w200/${movieCard.poster_path}`} alt={movieCard.title} />
-    <div className="overlay">
-      <p>{movieCard.title}</p>
-      <p>{movieCard.release_date}</p>
-      <p>{movieCard.vote_average}</p>
-      <p>{movieCard.overview}</p>
-    </div>
-  </div>
-</div>
-  )
+function truncateOverview(overview, maxLength) {
+  if (overview.length > maxLength) {
+    return overview.substring(0, maxLength) + "...";
+  }
+  return overview;
 }
 
-export default Movie
+const Movie = ({ movieCard }) => {
+  const truncatedOverview = truncateOverview(movieCard.overview, 100);
+
+  return (
+    <div className='popular-movie'>
+      <div className='movie-image-wrap'>
+        <img key={movieCard.id} src={`https://image.tmdb.org/t/p/w200/${movieCard.poster_path}`} alt={movieCard.title} />
+        <div className="overlay">
+          <p className='movie-card-title'>{movieCard.title}</p>
+          <p className='movie-card-release-date'>{movieCard.release_date}</p>
+          <p className='movie-card-average'>{movieCard.vote_average}</p>
+          <p className='movie-card-overv'>{truncatedOverview}</p>
+          <button className='more-info' role='button'>More Info</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
