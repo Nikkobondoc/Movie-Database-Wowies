3383b5ebecf77158cb5bb0c50f84baedb9f6bc94
Nikko <nikkobondoc1120@gmail.com>
2024-02-19 19:43:07 -0800

    added usestates

--- components/MovieDetail.jsx
+++ components/MovieDetail.jsx
@@ -1,43 +1,30 @@
-import React from 'react'; // Import React
+import React, { useState } from 'react';
 import useWindowDimensions from '../components/ScreenSize';
 import { dateFormat, minToHrMin, ratingAverage } from '../utilities/format';
 import AddFavourites from './AddFavourites';
 
-function MovieDetail({ movie, favouriteComponent }) { // Correct the prop destructuring
+function MovieDetail({ movie }) {
   const dimensions = useWindowDimensions();
   const desktopWidth = 1440;
-  let isDesktop = dimensions.width > desktopWidth;
+  const [favourites, setFavourites] = useState([]);
+  
+
+  const isDesktop = dimensions.width > desktopWidth;
+
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+  };
 
   return (
     <div>
-      {isDesktop ? (
-        // if is desktop....
-        <div>
-          <div className="movie-backdrop">
-            {movie.backdrop_path === null ? (
-              <img className="no-backdrop" src={noDisplay} alt="No Backdrop Poster" />
-            ) : (
-              <img className="backdrop-img" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} width="300" />
-            )}
-
-            {movie.poster_path === null ? (
-              <img className="no-poster" src={noPoster} alt="No poster available." />
-            ) : (
-              <img className="poster-mobile" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} width="145" />
-            )}
-          </div>
-        </div>
-      ) : (
-        <div>
-          <div className="movie-backdrop">
-            {movie.poster_path === null ? (
-              <img className="no-poster" src={noPoster} alt="No poster available." />
-            ) : (
-              <img className="poster-mobile" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} width="145" />
-            )}
-          </div>
-        </div>
-      )}
+      <div className="movie-backdrop">
+        {movie.backdrop_path === null ? (
+          <img className="no-backdrop" src={noDisplay} alt="No Backdrop Poster" />
+        ) : (
+          <img className="backdrop-img" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} width="300" />
+        )}
+      </div>
 
       <div className="movie-info-single">
         <div className="release-runtime-vote">
@@ -47,8 +34,11 @@ function MovieDetail({ movie, favouriteComponent }) { // Correct the prop destru
         </div>
         <h2>{movie.title}</h2>
         <p className="desc">{movie.overview}</p>
-        <p>Genre: {movie.genres.map(genres => genres.name).join(", ")}</p>
-        <AddFavourites /> {/* Use favouriteComponent directly */}
+        <p>Genre: {movie.genres.map(genre => genre.name).join(", ")}</p>
+        
+        <div onClick={() => addFavouriteMovie(movie)}>
+          <AddFavourites /> 
+        </div>
       </div>
     </div>
   );
