3683072bf0fd10e090654261cc4c80550399863d
Nikko <nikkobondoc1120@gmail.com>
2024-02-20 19:45:16 -0800

    changed favourite movie to display movie poster

--- pages/PageFavourite.jsx
+++ pages/PageFavourite.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Removefavourites from '../components/Removefavourites';
-// import '/src/styles/favourites.scss'; 
-import '../src/styles/favourites.scss'; 
+import '/src/styles/favourites.scss'; 
+
 
 const PageFavourite = () => {
   const [favouriteItems, setFavouriteItems] = useState([]);
@@ -24,7 +24,7 @@ const PageFavourite = () => {
       <h2>Favourite Page</h2>
       <div className='favourite-grid'>
         {favouriteItems.length === 0 ? (
-          <p>No Favourites. Please add some.<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown" viewBox="0 0 16 16">
+          <p>No Favourites. Please add some.<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="blacl" class="bi bi-emoji-frown" viewBox="0 0 16 16">
 		  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
 		  <path d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.5 3.5 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.5 4.5 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"/>
 		</svg></p>
@@ -32,11 +32,11 @@ const PageFavourite = () => {
         ) : (
           favouriteItems.map((movie, index) => (
             <div key={index} className='movie-card'>
-              <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} width="200" />
+              <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} width="200" />
               <button onClick={() => removeFavouritesMovie(movie.id)}><Removefavourites /></button>
               <div className="overlay">
+                {/* <h4>{movie.description}</h4> */}
                 <h3>{movie.title}</h3>
-                <h4>{movie.description}</h4>
               </div>
             </div>
           ))
